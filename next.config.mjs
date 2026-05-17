dfa62f8ce6a1a0babcb25750b52ea908be14be66
GSD Agent <gsd@example.com>
2026-05-17 13:29:48 -0300

    feat(core): configure strict environment variables

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// Import env files to validate at build time
+import "./src/env.mjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
